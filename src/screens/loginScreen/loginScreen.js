7cd7dced50c367b74ef4db083c070cb1492c2665
mb3668 <mb3668@msstate.edu>
2023-04-08 16:16:22 -0500

    updated firebase and added login functionality

--- src/screens/loginScreen/loginScreen.js
+++ src/screens/loginScreen/loginScreen.js
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { signIn } from '../../firebase';
 import './login.css';
 
 function LoginScreen() {
-    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
     const [userPassword, setUserPassword] = useState('');
 
     function handleUserChange(event) {
-        setUserName(event.target.value);
+        setUserEmail(event.target.value);
     }
 
     function handlePasswordChange(event) {
         setUserPassword(event.target.value);
     }
 
-    function handleUserSubmit(event) {
+    async function handleUserSubmit(event) {
         event.preventDefault();
-        console.log(`Username: ${userName} Password: ${userPassword}`);
+        console.log(`Username: ${userEmail} Password: ${userPassword}`);
+
+        try {
+            const user = await signIn(userEmail, userPassword);
+            console.log(user);
+            // Redirect here
+        } catch (error) {
+            console.log(error.message);
+        }
     }
 
     return (
@@ -29,12 +38,12 @@ function LoginScreen() {
 
                         {/* Username input field */}
                         <Form.Group controlId="formUsername">
-                            <Form.Label>Username</Form.Label>
+                            <Form.Label>Email</Form.Label>
                             <br></br>
                             <Form.Control className="userField"
-                                type="text"
-                                placeholder="Username"
-                                value={userName}
+                                type="email"
+                                placeholder="Email"
+                                value={userEmail}
                                 onChange={handleUserChange}
                             />
                         </Form.Group>
